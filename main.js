3b33aa57bc9a520192089096848fde55916610f2
gabibonardi <gabriela.bonardi@escola.pr.gov.br>
2026-06-18 18:36:16 -0300

    Fix countdown timer and button functionality

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-12-20T00:00:00");
 const tempoObjetivo2 = new Date("2026-11-01T14:00:00");
-const tempoObjetivo3 = new Date("2026-11-21T08:30:00");
+const tempoObjetivo3 = new Date("2026-08-23T08:30:00");
 const tempoObjetivo4 = new Date("2027-01-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
